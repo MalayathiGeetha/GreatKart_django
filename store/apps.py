c2aa6e5e29927ef6dfd42311222358fd758711a4
malayathigeetha <malayathigeetha@gmail.com>
2025-08-20 10:56:35 +0530

    store and carts functionalities

--- store/apps.py
+++ store/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
-class ShareConfig(AppConfig):
-    name = 'share'
+class StoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'store'   # ✅ correct app name
